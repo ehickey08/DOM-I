f6d6cf2e1a9a4767e7cd9e0faacb7d53099c0e72
ehickey08 <49757261+ehickey08@users.noreply.github.com>
2019-05-13 17:51:22 -0400

    Create start, pause, reset timers

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,4 +1,5 @@
 let allDigits = document.querySelectorAll('.digit');
+
 let tenthsCounter = 0;
 let hundredthsCounter = 0;
 let onesCounter = 0;
@@ -35,9 +36,91 @@ let count = function() {
         allDigits[1].textContent = 0;
         allDigits[3].textContent = 0;
         allDigits[4].textContent = 0;
-        allDigits.forEach (el => el.style.color = 'red');
+        allDigits.forEach (el => el.classList.add('redDigit'));
         window.clearInterval(myTimer);
-    } 
+        resetButton.disabled = false;
+        pauseButton.disabled = true; 
+        digitDiv.style.animation = 'bounce-in 1s ease-in';
+    } else {
+        allDigits[0].textContent = 0;
+    }
     
 }
-let myTimer = window.setInterval (count, 10);
+
+let startButton = document.createElement('button');
+let resetButton = document.createElement('button');
+let pauseButton = document.createElement('button');
+let buttonDiv = document.createElement('div');
+let digitDiv = document.querySelector('.digits');
+let body = document.querySelector('body');
+
+buttonDiv.appendChild(startButton);
+buttonDiv.appendChild(pauseButton);
+buttonDiv.appendChild(resetButton);
+
+let parentDiv = document.createElement('div');
+body.appendChild(parentDiv);
+parentDiv.appendChild(digitDiv);
+parentDiv.appendChild(buttonDiv);
+
+parentDiv.style.display = 'flex';
+parentDiv.style.flexDirection = 'column';
+parentDiv.style.width = '60%';
+parentDiv.style.marginTop = '10%';
+parentDiv.style.justifyContent = 'space-around';
+parentDiv.style.height = '40vh'
+digitDiv.style.textAlign = 'center';
+
+buttonDiv.style.display = 'flex';
+buttonDiv.style.justifyContent = 'space-between';
+
+let buttons = document.querySelectorAll('button');
+
+let buttonStyling = function(el) {
+    el.style.width = '20vh';
+    el.style.height = '8vh'
+    el.style.margin = '3%'
+}
+
+buttons.forEach (buttonStyling);
+startButton.textContent = 'Start Timer';
+resetButton.textContent = 'Reset Timer';
+pauseButton.textContent = 'Pause Timer';
+
+
+startButton.addEventListener('click', (event) => {
+    myTimer = window.setInterval (count, 10);
+    console.log('clicked');
+    startButton.disabled = true;
+    resetButton.disabled = true;
+});
+
+let paused = true;
+pauseButton.addEventListener('click', (event) => {
+    if(paused){
+        window.clearInterval(myTimer);
+        paused = false;
+        pauseButton.textContent = 'Resume Timer';
+        resetButton.disabled = false;
+    } else {
+        myTimer = window.setInterval (count, 10);
+        paused = true;
+        pauseButton.textContent = 'Pause Timer';
+        resetButton.disabled = true;
+    }
+});
+
+
+resetButton.addEventListener('click', (event) => {
+    allDigits.forEach(el => el.textContent = 0);
+    allDigits.forEach(el => el.classList.remove('redDigit'));
+    allDigits[2].textContent = ':'
+    tenthsCounter = 0;
+    hundredthsCounter = 0;
+    onesCounter = 0;
+    tensCounter = 0;
+    pauseButton.textContent = 'Pause Timer';
+    paused = true;
+    startButton.disabled = false;
+    pauseButton.disabled = false;
+});
